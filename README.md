aa03463e18378cdc8292a2ecb0f0bcd607ab8606
Hleb Bandarenka <hbanda@ext.uber.com>
2019-07-31 10:06:55 +0300

    add comments and update README

--- README.md
+++ README.md
@@ -1,15 +1,56 @@
-https://cloud.docker.com/u/kakawi/repository/docker/kakawi/grpc-gateway-container
+# Gateway for gRPC + protobuf through REST
+This project is created for development/test process.
+gRPC + protofuf work with binary protocol, so we can't use Postman to invoke any endpoint.
+To do it we should make a proxy and run.
+This application create proxies for all **.proto** files and run them.
 
-docker run -d --publish 9031:9031 -it -e START_DIR=<DIR_FOR_PROTO_FILES> -v <PROTO_FILES>:/proto/<DIR_FOR_PROTO_FILES> kakawi/grpc-gateway-container
+# Example
+There is **/example/my_service.proto**
+```
+syntax = "proto3";
+package example;
+message StringMessage {
+  string value = 1;
+}
 
-if you use on Mac add parameter
-`-e DESTINATION_HOST=host.docker.internal`
+service YourService {
+  rpc Echo(StringMessage) returns (StringMessage) {}
+}
+```
 
-## Parameters
-START_DIR - [.]
+We start
+On Linux
+```
+docker run -d --net=host -it -e START_DIR=example -v /example:/proto/example kakawi/grpc-gateway-container
+```
+On Mac
+```
+docker run -d --publish 9031:9031 -it -e START_DIR=example -e DESTINATION_HOST=host.docker.internal -v /example:/proto/example kakawi/grpc-gateway-container
+```
+
+And after it we can make a **POST** call through Postman on endpoint
+```
+http://localhost:9031/YourService/Echo
+
+{
+  "value": "Hello World"
+}
+```
+
+## Start container
+docker run -d --net=host -it -e START_DIR=<DIR_FOR_PROTO_FILES> -v <PROTO_FILES>:/proto/<DIR_FOR_PROTO_FILES> kakawi/grpc-gateway-container
+
+P.S. if you use Mac delete `--net=host` parameter and add parameters
+`--publish 9031:9031 -e DESTINATION_HOST=host.docker.internal`
+
+### Parameters
+START_DIR - [.] directory where script will look for **.proto** files
 
 DESTINATION_HOST - [localhost]
 
 DESTINATION_PORT - [8087]
 
 PROXY_PORT - [9031]
+
+## Docker Hub: 
+https://cloud.docker.com/u/kakawi/repository/docker/kakawi/grpc-gateway-container
--- generate_proxy_grpc.sh
+++ generate_proxy_grpc.sh
@@ -1,29 +1,44 @@
 #!/usr/bin/env bash
 
-cd /proto
 start_dir=${START_DIR}
 destination_host=${DESTINATION_HOST}
 destination_port=${DESTINATION_PORT}
 proxy_port=${PROXY_PORT}
 
+# In this file will be collected information from first part and use it in second part
 tmp_registration_file=$(mktemp /tmp/registration_file-XXXXX)
 
-# 1. compile all proto files
+# `protoc` can't handle relatives path correctly, so we should start from a proper folder
+# all our *.proto files should be under `/proto` folder
+cd /proto
+
+# `protoc` can't pass recursively through folders to find all *.proto files,
+# so we do it instead of it
 for current_dir in $(find ${start_dir} -type d); do
+  # handle only *.proto files in current folder (-maxdepth 1)
   if [[ -n $(find "${current_dir}" -maxdepth 1 -name "*.proto") ]]; then
     for current_proto_file in $(find ${current_dir} -maxdepth 1 -name "*.proto"); do
+      # 1. compile `*.proto` file in *.go file and put in `${GOPATH}/src` folder
       protoc -I. \
         -I${GOPATH}/src \
         -I${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
         --go_out=plugins=grpc:${GOPATH}/src \
         ${current_proto_file}
 
-      #2. compile all gateways + yaml settings
+      # 2. if this `*.proto` file contains a work `service`, so we should also create
+      # proxy (gateway) for this service
+      # Ref: https://github.com/grpc-ecosystem/grpc-gateway#usage
       if [[ -n $(grep "service" ${current_proto_file}) ]]; then
 
-        #2.1 Generate yaml file
-        tmpfile=$(mktemp /tmp/http_rules-XXXX.yaml)
+        #2.1 - Generate Proxy
+        # To generate proxy we should add `google.api.http` annotation, but we don't want to add them
+        # in proto files, so we can create gRPC Service Configuration file
+        # Ref: https://cloud.google.com/endpoints/docs/grpc/grpc-service-config
+        grpc_service_configuration_file=$(mktemp /tmp/grpc-service-configuration-XXXX.yaml)
 
+        # Gather names to generate gRPC Service Configuration file
+        # Every service will be have such http rules
+        # /<SERVICE_NAME>/<METHOD_NAME>
         #2.1.1 Get package name
         package_name=$(grep -Eo "^package .*;" ${current_proto_file} | cut -d" " -f2 | cut -d";" -f1)
         #2.1.2 Get service name
@@ -31,35 +46,35 @@ for current_dir in $(find ${start_dir} -type d); do
         #2.1.3 Get method names
         methods=($(grep -Eo "rpc .*\(" ${current_proto_file} | cut -d" " -f2 | cut -d"(" -f1))
 
-        #2.2 Write the head of file
-        cat <<-EOF >${tmpfile}
+        #2.1.4 Write the head of file
+        cat <<-EOF >${grpc_service_configuration_file}
 type: google.api.Service
 config_version: 3
 
 http:
   rules:
 EOF
+        #2.1.5 Add http rules
         for method_name in "${methods[@]}"; do
-          cat <<-EOF >>${tmpfile}
+          cat <<-EOF >>${grpc_service_configuration_file}
     - selector: ${package_name}.${service_name}.${method_name}
       post: /${service_name}/${method_name}
       body: "*"
 EOF
         done
 
-        #2.2 Use this file to generate Gateway
+        #2.2 Use this gRPC Service Configuration to generate Proxy (*.gw.go file) and put it under `${GOPATH}/src`
         protoc -I. \
           -I${GOPATH}/src \
           -I${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-          --grpc-gateway_out=logtostderr=true,grpc_api_configuration=${tmpfile}:${GOPATH}/src \
+          --grpc-gateway_out=logtostderr=true,grpc_api_configuration=${grpc_service_configuration_file}:${GOPATH}/src \
           ${current_proto_file}
 
-        #2.3 Add Registation in proxy file
-
+        #2.3 Save `dirname of proto file`|`service name` in separate file to use it in next Go file
+        # which will start all proxies altogether
         echo "$(dirname ${current_proto_file})|${service_name}" >>${tmp_registration_file}
 
-        #rm "${tmpfile}"
-        #echo "Service generated"
+        echo "Service ${service_name} generated"
       fi
     done
   fi
@@ -67,9 +82,10 @@ done
 
 echo "All Proto files converted in Go"
 
-#3. make a proxy with all gateways
+# 3. Generate Go script which will start all Proxies altogether
 proxy_grpc_script=${PROXY_EXEC_FILE}
-# First part
+
+# 3.1 Add head of file
 cat <<-EOF >${proxy_grpc_script}
 package main
 
@@ -83,7 +99,7 @@ import (
   "google.golang.org/grpc"
 EOF
 
-# Imports
+# 3.2 Add imports as template `gateway<INDEX> <SERVICE_DIR>`
 index=0
 for service_row in $(cat ${tmp_registration_file}); do
   service_package=$(echo ${service_row} | cut -d"|" -f1)
@@ -91,7 +107,7 @@ for service_row in $(cat ${tmp_registration_file}); do
   index=$((index+1))
 done
 
-# Second part
+# 3.3 Add second part with `destination_host` and `destination_port`
 cat <<-EOF >>${proxy_grpc_script}
 )
 
@@ -112,7 +128,8 @@ func run() error {
   opts := []grpc.DialOption{grpc.WithInsecure()}
 EOF
 
-# Registation part
+# 3.4 Register all proxies like:
+# gateway<INDEX>.<SERVICE_NAME>HandlerFromEndpoint(ctx, mux,  *grpcServerEndpoint, opts)
 index=0
 for service_row in $(cat ${tmp_registration_file}); do
   service_name=$(echo ${service_row} | cut -d"|" -f2)
@@ -121,7 +138,7 @@ for service_row in $(cat ${tmp_registration_file}); do
   index=$((index+1))
 done
 
-# Last Part
+# 3.5 Add final part with `proxy_port`
 cat <<-EOF >>${proxy_grpc_script}
 // Start HTTP server (and proxy calls to gRPC server endpoint)
   return http.ListenAndServe(":${proxy_port}", mux)
@@ -136,5 +153,3 @@ func main() {
   }
 }
 EOF
-
-rm ${tmp_registration_file}
